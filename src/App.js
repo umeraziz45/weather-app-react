c76e92cb06f18d350a743e30f96d1abd3c1075f5
Umer <umeraziz4545@gmail.com>
2022-01-25 08:59:26 -0700

    axios call successful

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [ apiCall, setApiCall ] = useState({});
+
+  const apiKey = '7bdbfb7799ae2ccaf862515d0e38d2ea';
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  useEffect( () => {
+    axios({
+      url: apiUrl,
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        q: 'London',
+        appid: apiKey,
+      }
+    }).then( (json) => json.data)
+      .then( (jsonData) => console.log(jsonData))
+      .catch( (error) => console.log(error))
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
